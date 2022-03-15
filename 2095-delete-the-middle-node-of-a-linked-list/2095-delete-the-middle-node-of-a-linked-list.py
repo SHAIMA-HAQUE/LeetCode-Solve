2a2f02dfc59c486793dee71a5ad49fca568db83d
Shaima Haque <71368232+SHAIMA-HAQUE@users.noreply.github.com>
2022-03-15 22:03:40 +0530

    Time: 3104 ms (15.92%), Space: 60.7 MB (60.32%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -5,22 +5,40 @@
 #         self.next = next
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow, fast = head, head
+        #Approach2
+        temp = head
         if head is None:
             return head
         if head.next is None:
             return None
         c = 0
-        while fast and fast.next:
+        while temp is not None:
             c += 1
-            if c == 1:
-                prev = head
-                slow = slow.next
-                fast = fast.next.next
-            else:
-                prev = prev.next
-                slow = slow.next
-                fast = fast.next.next
-        prev.next = slow.next
-        
+            temp = temp.next
+        temp = head
+        for i in range(c//2-1):
+            temp = temp.next
+        temp.next = temp.next.next
         return head
+        #Approach 1
+#         slow, fast = head, head
+#         if head is None:
+#             return head
+#         if head.next is None:
+#             return None
+#         c = 0
+#         while fast and fast.next:
+#             c += 1
+#             if c == 1:
+#                 prev = head
+#                 slow = slow.next
+#                 fast = fast.next.next
+#             else:
+#                 prev = prev.next
+#                 slow = slow.next
+#                 fast = fast.next.next
+#         prev.next = slow.next
+        
+#         return head
+
+        
