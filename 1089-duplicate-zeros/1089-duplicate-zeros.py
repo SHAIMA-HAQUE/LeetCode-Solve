736c1b701a319668c9250f49c163bf4d4547227a
Shaima Haque <71368232+SHAIMA-HAQUE@users.noreply.github.com>
2022-03-18 11:17:55 +0530

    Time: 223 ms (17.08%), Space: 14.8 MB (87.44%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -3,13 +3,21 @@ class Solution:
         """
         Do not return anything, modify arr in-place instead.
         """
-        length = 0
-        for i in range(len(arr)):
-            if length < len(arr):
-                if arr[length] == 0:
-                    arr.pop()
-                    arr.insert(length+1,0)
-                    length += 2
-                else:
-                    length += 1
+        # length = 0
+        # for i in range(len(arr)):
+        #     if length < len(arr):
+        #         if arr[length] == 0:
+        #             arr.pop()
+        #             arr.insert(length+1,0)
+        #             length += 2
+        #         else:
+        #             length += 1
+        # return arr
+        n= len(arr)
+        i = 0
+        while i < n:
+            if arr[i] == 0:
+                arr[i+1:n] = arr[i:n-1]
+                i +=2
+            else: i+=1
         return arr
