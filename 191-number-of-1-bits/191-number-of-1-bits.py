a244a5736ce6e43f057911686c942c939812f9d8
Shaima Haque <71368232+SHAIMA-HAQUE@users.noreply.github.com>
2022-03-24 17:34:45 +0530

    Update 191-number-of-1-bits.py

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,7 +1,13 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+#         c = 0
+#         for i in range(32):
+#             if n & (1<<i)!= 0:
+#                 c +=1
+#         return c
         c = 0
-        for i in range(32):
-            if n & (1<<i)!= 0:
-                c +=1
+        while n>0:
+            if n&1 > 0:
+                c += 1
+            n = n >> 1
         return c
