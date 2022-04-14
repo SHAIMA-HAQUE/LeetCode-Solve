5cd094aa036a342cecdfeac3c32609ab4e0519c4
Shaima Haque <71368232+SHAIMA-HAQUE@users.noreply.github.com>
2022-04-14 17:38:35 +0530

    Time: 3 ms (95.59%), Space: 8.5 MB (21.73%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res;
+        for(int i = 0; i<=n; i++){
+            int ans = 0;
+            int j = i;
+            while(j>0){
+                j = j &(j -1);
+                ans ++;
+            }
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
