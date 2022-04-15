afb8ac8f86142ede8c54bf3c25a3c7a042f91c1d
Shaima Haque <71368232+SHAIMA-HAQUE@users.noreply.github.com>
2022-04-15 22:27:33 +0530

    Time: 32 ms (88.65%), Space: 13.9 MB (60.31%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -1,8 +1,6 @@
-class Solution(object):
-    def average(self, salary):
-        """
-        :type salary: List[int]
-        :rtype: float
-        """
-        avg = (sum(salary) - max(salary) - min(salary))/(len(salary)-2.0)
-        return avg
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        total_sum = sum(salary)
+        mini = min(salary)
+        maxi = max(salary)
+        return (total_sum - (maxi+mini))/(len(salary)-2)
