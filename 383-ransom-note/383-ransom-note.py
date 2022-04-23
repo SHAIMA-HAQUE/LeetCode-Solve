840e5bebe812986e4a5d4bfc2fdf93ff4d18dcaa
Shaima Haque <71368232+SHAIMA-HAQUE@users.noreply.github.com>
2022-04-23 10:32:56 +0530

    Time: 47 ms (92.22%), Space: 14.1 MB (94.39%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,6 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for i in set(ransomNote):
+            if ransomNote.count(i) > magazine.count(i):
+                return False
+        return True
