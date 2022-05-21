b8789bfb3d37e7cd8d9e6f00afea641cdee46d6d
Shaima Haque <71368232+SHAIMA-HAQUE@users.noreply.github.com>
2022-05-21 17:37:47 +0530

    Time: 38 ms (5.18%), Space: 20.6 MB (5.03%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
+        if(n<=2){
+            return 0;
+        }
         vector<int>left(n, 0), right(n, 0);
         left[0] = height[0];
         right[n-1] = height[n-1];
