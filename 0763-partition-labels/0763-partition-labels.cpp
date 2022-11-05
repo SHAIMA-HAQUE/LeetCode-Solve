45aa13b798698a2664bd95403975d3e9303faf8d
Shaima Haque <71368232+SHAIMA-HAQUE@users.noreply.github.com>
2022-11-05 20:55:31 +0530

    Time: 3 ms (92.57%), Space: 6.7 MB (68.56%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> end_idx(26,0);
+        
+        for(int i = 0; i < s.length(); ++i)
+            end_idx[s[i] - 'a']  = i;
+        
+        vector<int> res;
+        
+        int start = 0, end = 0;
+		
+        for(int i = 0; i < s.length(); ++i)
+        {
+            end = max(end, end_idx[s[i] - 'a']);
+            if( i == end)
+            {
+                res.push_back(i - start + 1);
+                start = i + 1;
+            }
+        }
+        
+        return res;
+    }
+};
